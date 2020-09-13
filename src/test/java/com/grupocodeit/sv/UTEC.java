d6b47a998e114b28831a2ed7190adcd7ec8304dc
Erick Cruz <mail22master@gmail.com>
2020-09-13 01:41:20 -0600

    3 casos de prueba

--- src/test/java/com/grupocodeit/sv/UTEC.java
+++ src/test/java/com/grupocodeit/sv/UTEC.java
@@ -15,90 +15,83 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import junit.framework.Assert;
 
 public class UTEC {
-	//Preparar la instancia del web driver
+	// Preparar la instancia del web driver
 	private WebDriver driver;
 
 	@Before
 	public void setUp() throws Exception {
-		//asignar web driver a nuestro proyecto
-		System.setProperty("webdriver.chrome.driver", "./src/test/chromedriver/chromedriver.exe"); 
-		
-		//istancia de la clase
+		// asignar web driver a nuestro proyecto
+		System.setProperty("webdriver.chrome.driver", "./src/test/chromedriver/chromedriver.exe");
+		// istancia de la clase
 		driver = new ChromeDriver();
-		
-		//maximizar la ventana del navegador
+		// maximizar la ventana del navegador
 		driver.manage().window().maximize();
-		
-		//abrir la pagina web
+		// abrir la pagina web
 		driver.get("https://portal.utec.edu.sv/");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//cerrar el navegador
+		// cerrar el navegador
 		driver.quit();
 	}
 
-	//Elementos mapeados
+	// Elementos mapeados
 	By txtUser = By.id("UsuarioTxt");
 	By txtPass = By.id("ContrasenaTxt");
 	By btnLogin = By.name("BtnEntrar");
-	
-	By lblMensaje = By.id("msj");	
-	String mensajeLoginIncorrecto="* Cuenta inactiva o ingreso mal su usuario o contraseña, intente nuevamente.";
-	
+
+	By lblMensaje = By.id("msj");
+	String mensajeLoginIncorrecto = "* Cuenta inactiva o ingreso mal su usuario o contraseña, intente nuevamente.";
+
 	By tableError = By.id("ValidatorCalloutExtender2_popupTable");
 	String mensajeDatosvacios = "El usuario es requerido!";
-	
+
+	// Usuario y contraseñas vacios
 	@Test
-	public void loginUtec() throws InterruptedException {
-		LoginEmpty();
-		loginError();
-		loginSuccess();				
-	}
-	
-	//Usuario y contraseñas vacios
 	public void LoginEmpty() throws InterruptedException {
 		driver.findElement(txtUser).sendKeys(" ");
 		driver.findElement(txtPass).sendKeys(" ");
 		driver.findElement(btnLogin).click();
 		Thread.sleep(1000);
-		if(driver.findElement(tableError).isDisplayed()) {			
-			if(driver.findElement(tableError).getText().equals(mensajeDatosvacios)) {
-				System.out.println("Exito: Usuario y contraseña vacíos");				
-			}else {
+		if (driver.findElement(tableError).isDisplayed()) {
+			if (driver.findElement(tableError).getText().equals(mensajeDatosvacios)) {
+				System.out.println("Exito: Usuario y contraseña vacíos");
+			} else {
 				System.out.println("Fallo: el mensaje mostrado es diferente");
-			}			
-		}else {
+			}
+		} else {
 			System.out.println("Falló: La tabla de validacion de los datos vacios no se motró");
-		}		
+		}
 	}
-	
-	//Usuario y contraseñas incorrectos
+
+	// Usuario y contraseñas incorrectos
+	@Test
 	public void loginError() throws InterruptedException {
 		driver.findElement(txtUser).sendKeys("987654321");
 		driver.findElement(txtPass).sendKeys("123456789");
 		driver.findElement(btnLogin).click();
 		Thread.sleep(1000);
-		if(driver.findElement(lblMensaje).isDisplayed()) {			
-			if(driver.findElement(lblMensaje).getText().equals(mensajeLoginIncorrecto)) {				
+		if (driver.findElement(lblMensaje).isDisplayed()) {
+			if (driver.findElement(lblMensaje).getText().equals(mensajeLoginIncorrecto)) {
 				assertEquals(mensajeLoginIncorrecto, driver.findElement(lblMensaje).getText());
 				System.out.println("Exito: Usuario y/o contraseñas incorrectas");
-			}else {
+			} else {
 				assertEquals(mensajeLoginIncorrecto, driver.findElement(lblMensaje).getText());
 				System.out.println("Fallo: mensaje incorrecto");
 			}
-			
-		}else {
+
+		} else {
 			System.out.println("Fallo: El Div no se mostró");
 		}
 	}
-	
-	//Usuario y contraseñas correctos
+
+	// Usuario y contraseñas correctos
+	@Test
 	public void loginSuccess() throws InterruptedException {
 		driver.findElement(txtUser).sendKeys("USUARIO_CORRECTO");
-		driver.findElement(txtPass).sendKeys("PASS_CORRECTA");
-		driver.findElement(btnLogin).click();		
+		driver.findElement(txtPass).sendKeys("PASS_CORRECTO");
+		driver.findElement(btnLogin).click();
 	}
 
 }
